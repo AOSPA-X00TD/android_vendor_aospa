3269f8f60f95a6a1bd191e65ee1027dbba69d9c4
wanderlusttt2 <98012371+wanderlusttt2@users.noreply.github.com>
2022-08-02 14:08:17 +0530

    Fix $USER in ci-builds
    Kanged from: https://github.com/wHo-EM-i/android_vendor_aospa/commit/c4f2f4a01e884283600cda595332774f1fd041cd

--- build.sh
+++ build.sh
@@ -196,9 +196,12 @@ fi
 echo -e "${CLR_BLD_BLU}Starting compilation${CLR_RST}"
 echo -e ""
 
-# If we aren't in Jenkins, use the engineering tag
 if [ -z "${BUILD_NUMBER}" ]; then
-    export FILE_NAME_TAG=eng.$USER
+    if [ -z "${USER}" ]; then
+        export FILE_NAME_TAG=eng.root
+    else
+        export FILE_NAME_TAG=eng.$USER
+    fi
 else
     export FILE_NAME_TAG=$BUILD_NUMBER
 fi
@@ -280,7 +283,7 @@ else
 
     checkExit
 
-    cp -f $OUT/aospa_$DEVICE-ota-$FILE_NAME_TAG.zip $OUT/aospa-$AOSPA_VERSION.zip
+    mv -f $OUT/aospa_$DEVICE-ota-$FILE_NAME_TAG.zip $OUT/aospa-$AOSPA_VERSION.zip
     echo "Package Complete: $OUT/aospa-$AOSPA_VERSION.zip"
 fi
 echo -e ""
